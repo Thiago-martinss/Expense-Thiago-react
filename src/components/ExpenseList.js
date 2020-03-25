a6a7d7f9cc58b5b6521f82131ef56708688b6a41
Thiago <martinsmthiago@gmail.com>
2020-03-25 01:36:42 -0300

    implementado css no expense list + pagina loading

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import ExpenseListItem from './ExpenseListItem'
-import selectExpenses from '../selectors/expenses'
+import React from "react";
+import { connect } from "react-redux";
+import ExpenseListItem from "./ExpenseListItem";
+import selectExpenses from "../selectors/expenses";
 
-
-
- export const ExpenseList = (props) => (
-    <div>
-    {
-        props.expenses.length === 0 ? (
-            <p>Sem despesas registrada</p>
-        ) : (
-            props.expenses.map((expense) => {
-                return <ExpenseListItem key={expense.id} {...expense}/>
-            })
-        
-        )
-    }
-     
+export const ExpenseList = props => (
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Despesas</div>
+      <div className="show-for-desktop">Despesa</div>
+      <div className="show-for-desktop">Quantia</div>
     </div>
-)
+        <div className="list-body">
+    
+    {props.expenses.length === 0 ? (
+      <div className=" list-item list-item--message">
+        <span>Sem despesas registrada</span>
+      </div>
+    ) : (
+      props.expenses.map(expense => {
+        return <ExpenseListItem key={expense.id} {...expense} />;
+      })
+    )}
+        </div>
+  </div>
+    
+);
 
-const mapStateToProps = (state) => {
-    return {
-     expenses: selectExpenses(state.expenses, state.filters)
-    }
-}
+const mapStateToProps = state => {
+  return {
+    expenses: selectExpenses(state.expenses, state.filters)
+  };
+};
 
-export default connect(mapStateToProps)(ExpenseList) 
-
+export default connect(mapStateToProps)(ExpenseList);
